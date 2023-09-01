3e7f4c976cc0e476989dd582d53d3e100ed349ab
Jon Haataja <jon.haataja@gmail.com>
2023-09-01 20:08:42 +0300

    Added initial game logic (user can guess, rounds are being printed, game can be stopped with "stop")

--- JavaGuessingGame/src/app/GuessingGame.java
+++ JavaGuessingGame/src/app/GuessingGame.java
@@ -1,10 +1,31 @@
 package app;
 
+import java.util.Scanner;
+
 public class GuessingGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		String userGuess;
+		int rounds = 0;
+		
+		do
+		{
+			System.out.println("Guess my name (type stop to exit)");
+			userGuess = in.nextLine();
+			rounds = rounds + 1;
+			if(userGuess.equals("Emma")) {
+				System.out.println("Congratulations!");
+				System.out.printf("You guessed %d times.", rounds);
+			}
+			if(userGuess.equals("stop")) {
+				System.out.printf("You guessed %d times.", rounds-1);
+				break;
+			}
+			
+		}
+		while(!userGuess.equals("Emma"));
 	}
 
 }
