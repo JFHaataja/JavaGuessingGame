ec5ab9111ff94df4ad97f5d97998dc35057aef04
Jon Haataja <jon.haataja@gmail.com>
2023-09-01 21:00:51 +0300

    Added maximum rounds (4) and user hints, modified logic.

--- JavaGuessingGame/src/app/GuessingGame.java
+++ JavaGuessingGame/src/app/GuessingGame.java
@@ -9,23 +9,40 @@ public class GuessingGame {
 		
 		String userGuess;
 		int rounds = 0;
-		
+		String correctName = "Emma";
+		String firstLetter = correctName.substring(0, 1);
+		String secondLetter = correctName.substring(1, 2);
+		String thirdLetter = correctName.substring(2, 3);
+
 		do
 		{
+
 			System.out.println("Guess my name (type stop to exit)");
 			userGuess = in.nextLine();
 			rounds = rounds + 1;
-			if(userGuess.equals("Emma")) {
+			if (userGuess.equals(correctName))
+			{
 				System.out.println("Congratulations!");
 				System.out.printf("You guessed %d times.", rounds);
-			}
-			if(userGuess.equals("stop")) {
-				System.out.printf("You guessed %d times.", rounds-1);
 				break;
 			}
-			
+			else if(userGuess.equals("stop")){
+				break;
+			}
+			else if(!userGuess.equals(correctName) && rounds == 1){
+				System.out.printf("A hint: The first letter of the correct name is %s%n", firstLetter);
+
+			}else if(!userGuess.equals(correctName) && rounds == 2){
+				System.out.printf("Second hint: The second letter of the correct name is %s%n", secondLetter);
+
+			}else if (!userGuess.equals(correctName) && rounds == 3){
+				System.out.printf("Third hint: The third letter of the correct name is %s%n", thirdLetter);
+			}else {
+				System.out.println("The correct answer is Emma!");
+				System.out.printf("You guessed %d times.", rounds);
+			}		
 		}
-		while(!userGuess.equals("Emma"));
+		while(userGuess.equals("stop") || rounds < 4);
 	}
 
 }
